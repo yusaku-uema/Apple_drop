299e32bdd0155704f61638427eaced2ac840ebd0
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2022-07-12 09:39:10 +0900

    Revert "もう一回追加"
    This reverts commit 4bd0ddf2e3f7dbe837cb652170c32522096cbf68.

--- apple_drop/main.cpp
+++ apple_drop/main.cpp
@@ -46,7 +46,6 @@ int g_teki;
 int g_Applec; //タイトルカーソル変数　消さないで
 
 int g_StageBGM; //mainのBGM追加します
-int g_SE1;
 //追加します
 int g_ky;
 
@@ -230,10 +229,8 @@ void DrawGameTitle(void) {
         if (--MenuNo < 0)MenuNo = 3;
     }
     //zキーでメニュー選択
-    if (g_KeyFlg & PAD_INPUT_A) {
-        PlaySoundMem(g_SE1, DX_PLAYTYPE_BACK, TRUE);
-        g_GameState = MenuNo + 1;
-    }
+    if (g_KeyFlg & PAD_INPUT_A)g_GameState = MenuNo + 1;
+
     //タイトル画像表示
     DrawGraph(0, 0, g_TitleImage, FALSE);
 
@@ -504,7 +501,6 @@ int LoadSounds() {
 
     //ステージBGMデータの読み込み
     if ((g_StageBGM = LoadSoundMem("sounds/Chapter9/MusMus-BGM-104.wav")) == -1)return -1;
-    if ((g_SE1 = LoadSoundMem("sounds/Chapter9/sentaku.wav")) == -1)return -1;
 }
 /***********************************************
  * ランキング並び替え
@@ -636,4 +632,3 @@ void UIView(void)
     SetFontSize(45);
     DrawString(510, 320, "SCORE", 0xffffff, 0);
 }
-
