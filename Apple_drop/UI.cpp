7082b35690564e83f2a17b83c24e226d31e756be
erimaki1844 <106945196+erimaki1844@users.noreply.github.com>
2022-07-19 11:42:15 +0900

    ポーズ画面バグ修正

--- Apple_drop/UI.cpp
+++ Apple_drop/UI.cpp
@@ -3,7 +3,7 @@
 #include"common.h"
 #include"BGMandSE.h"
 UI::UI() {
-    TIMELIMIT = 31000;
+   
 }
 
 void UI::TimeCount()
@@ -53,6 +53,7 @@ void UI::UIView()
 void UI::UIInit() {
     //åªç›ÇÃåoâﬂéûä‘ÇìæÇÈ
     g_StartTime = GetNowCount();
+    TIMELIMIT = 31000;
 }
 
 void UI::StopTime() {
